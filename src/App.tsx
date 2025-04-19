25da0cbfa2a6e5f678425ff3be608743256fe0d6
maarrrkkk <johnmarkaguilar405@gmail.com>
2025-04-19 20:20:35 +0800

    fixed problem displaying footer when changing page, added scrolltotop.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import './media.css';
 import Homepage from './pages/Homepage';
 import Aboutme from './pages/Aboutme';
 import Thesis from './pages/Thesis';
+import Projects from './pages/Projects';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Nav />
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="/aboutme" element={<Aboutme />} />
         <Route path="/thesis" element={<Thesis />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,8 @@
+function Projects() {
+    return (
+        <div className="bg-slate-950 h-screen place-content-center">
+            <h1 className="text-white text-center text-4xl font-bold">Projects</h1>
+        </div>
+    );
+}
+export default Projects;
